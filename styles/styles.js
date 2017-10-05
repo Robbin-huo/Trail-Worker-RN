fb18fa6cc431acc1512452af9f5a4629dd6fce5c
John Simoni <email": null>
2017-10-05 16:29:53 -0700

    Better styling of new trail request page

--- styles/styles.js
+++ styles/styles.js
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     marginTop: 10,
     color: 'white',
     fontWeight: 'bold',
-    padding: 3
+    padding: 3,
   },
   content: {
     flexDirection: 'column',
@@ -18,8 +18,21 @@ export const styles = StyleSheet.create({
     marginRight: 5
   },
   input: {
-    width: 200,
-    fontSize: 20
+    width: 300,
+    fontSize: 20,
+    backgroundColor: 'white'
+  },
+  lg_input: {
+    fontSize: 30,
+    flex: 1,
+    height: 150,
+    backgroundColor: 'white'
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15
   },
   titleText: {
     fontSize: 40,
